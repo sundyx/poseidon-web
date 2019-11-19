57021a806afdc264a70d5e74aace0b48c2894b6c
sundyx <sandy.xie@cfacorp.com>
2019-11-19 18:13:51 +0800

    generates CSV file

--- src/models/sensorData.ts
+++ src/models/sensorData.ts
@@ -2,6 +2,60 @@ export class SensorData {
   private _timeStamp: string;
   private _waterTemp: number;
   private _airTemp: number;
+  private _pH: number;
+  private _conductivity: number;
+  private _dissolvedOxygen: number;
+  private _oxidation: number;
+  private _ammonium: number;
+  private _deviceID: string;
+
+  get pH(): number {
+    return this._pH;
+  }
+
+  set pH(value: number) {
+    this._pH = value;
+  }
+
+  get conductivity(): number {
+    return this._conductivity;
+  }
+
+  set conductivity(value: number) {
+    this._conductivity = value;
+  }
+
+  get dissolvedOxygen(): number {
+    return this._dissolvedOxygen;
+  }
+
+  set dissolvedOxygen(value: number) {
+    this._dissolvedOxygen = value;
+  }
+
+  get oxidation(): number {
+    return this._oxidation;
+  }
+
+  set oxidation(value: number) {
+    this._oxidation = value;
+  }
+
+  get ammonium(): number {
+    return this._ammonium;
+  }
+
+  set ammonium(value: number) {
+    this._ammonium = value;
+  }
+
+  get deviceID(): string {
+    return this._deviceID;
+  }
+
+  set deviceID(value: string) {
+    this._deviceID = value;
+  }
 
   get timeStamp(): string {
     return this._timeStamp;
